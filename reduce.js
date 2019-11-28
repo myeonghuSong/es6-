03d56bf715aefbd4ba3b7c5a47dff639471df5aa
myeonghuSong <21300390@handong.edu>
2019-11-29 03:47:57 +0900

    reduce 2

--- reduce.js
+++ reduce.js
@@ -1,3 +1,11 @@
+const products = [
+    { name: '반팔티', price: 15000 },
+    { name: '긴팔티', price: 20000 },
+    { name: '핸드폰케이스', price: 15000 },
+    { name: '후드티', price: 30000 },
+    { name: '바지', price: 25000 },
+];
+
 const nums = [1, 2, 3, 4, 5];
 
 let total = 0;
@@ -25,3 +33,8 @@ console.log(reduce(add, 0, [1, 2, 3, 4, 5]));
 console.log(add(add(add(add(add(0,1), 2), 3), 4), 5) );
 
 console.log(reduce(add, [1,2,3,4,5]));
+
+console.log(reduce(
+    (total_price, products) => total_price + products.price,
+    0,
+    products));
